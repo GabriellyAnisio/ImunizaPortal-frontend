6919273a98f460fd5b7151ee0e1d42c27443d71d
Maria Gabrielly <mgabriellyanisio@gmail.com>
2024-07-19 00:33:30 -0300

    :sparkles: feat: Connecting backend to AppointmentForm page

--- src/components/AppointmentForm.jsx
+++ src/components/AppointmentForm.jsx
@@ -1,61 +1,107 @@
-import React from 'react';
 import { Input, Button, Box, Stack, FormControl, FormLabel } from '@chakra-ui/react';
-import DatePicker from 'react-datepicker'; 
+import DatePicker from 'react-datepicker';
+import { useForm, Controller } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from '../services/api'; 
+import 'react-datepicker/dist/react-datepicker.css';
+
+const schema = z.object({
+  name: z
+    .string()
+    .nonempty({ message: 'Name is required' })
+    .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/, { message: 'Name must only contain letters and spaces' })
+    .refine((name) => name.length >= 3, { message: 'Name must be real' }),
+  birthdate: z.instanceof(Date, { message: 'Birthdate is required' }),
+  dateTime: z.instanceof(Date, { message: 'Schedule day and time is required' }),
+});
 
 const AppointmentSubmit = () => {
-  const [birthdate, setBirthdate] = React.useState(null);
-  const [selectedDate, setSelectedDate] = React.useState(null); 
+  const { handleSubmit, control, formState: { errors, isValid }, register } = useForm({
+    mode: 'onChange',
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: '',
+      birthdate: null,
+      dateTime: null,
+    },
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({
-      name: e.target.name.value,
-      birthdate,
-      appointmentDateTime: `${selectedDate}`,
-    });
+  const onSubmit = async (data) => {
+    const formData = {
+      name: data.name,
+      birthDate: data.birthdate.toISOString().split('T')[0],
+      dateTime: data.dateTime.toISOString(), 
+    };
+
+    try {
+      const response = await axios.post('/api/appointment', formData);
+      console.log('Appointment created successfully:', response);
+    } catch (error) {
+      console.error('Error creating appointment:', error);
+    }
   };
 
   return (
     <Box>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Stack spacing={4}>
-            {/* Name */}
-            <FormControl id="name" isRequired>
+          {/* Name */}
+          <FormControl id="name" isRequired isInvalid={!!errors.name}>
             <FormLabel>Name</FormLabel>
-            <Input type="text" name="name" placeholder="Enter name" />
-            </FormControl>
+            <Input 
+              type="text" 
+              placeholder="Enter name" 
+              {...register('name')} 
+            />
+            {errors.name && <Box color="red.500">{errors.name.message}</Box>}
+          </FormControl>
 
-            {/* Birthdate */}
-            <FormControl id="birthdate" isRequired>
+          {/* Birthdate */}
+          <FormControl id="birthdate" isRequired isInvalid={!!errors.birthdate}>
             <FormLabel>Birthdate</FormLabel>
-            <DatePicker
-                selected={birthdate}
-                onChange={(date) => setBirthdate(date)}
-                dateFormat="dd/MM/yyyy"
-                placeholderText="Select date of birth"
+            <Controller
+              name="birthdate"
+              control={control}
+              render={({ field }) => (
+                <DatePicker
+                  selected={field.value}
+                  onChange={(date) => field.onChange(date)}
+                  dateFormat="dd/MM/yyyy"
+                  placeholderText="Select date of birth"
+                />
+              )}
             />
-            </FormControl>
+            {errors.birthdate && <Box color="red.500">{errors.birthdate.message}</Box>}
+          </FormControl>
 
-            {/* Schedule day and time */}
-            <FormControl id="appointmentDateTime" isRequired>
+          {/* Schedule day and time */}
+          <FormControl id="dateTime" isRequired isInvalid={!!errors.dateTime}>
             <FormLabel>Schedule day and time</FormLabel>
-            <DatePicker
-                selected={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-                showTimeSelect
-                timeFormat="HH:mm"
-                timeIntervals={60}
-                dateFormat="dd/MM/yyyy HH:mm"
-                placeholderText="Select the day and time"
+            <Controller
+              name="dateTime"
+              control={control}
+              render={({ field }) => (
+                <DatePicker
+                  selected={field.value}
+                  onChange={(date) => field.onChange(date)}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={60}
+                  dateFormat="dd/MM/yyyy HH:mm"
+                  placeholderText="Select the day and time"
+                />
+              )}
             />
-            </FormControl>
+            {errors.dateTime && <Box color="red.500">{errors.dateTime.message}</Box>}
+          </FormControl>
 
-            {/* Submit button */}
-            <Button type="submit" colorScheme="blue" size="lg">
+          {/* Submit button */}
+          <Button type="submit" colorScheme="blue" size="lg" isDisabled={!isValid}>
             Submit
-            </Button>
+          </Button>
         </Stack>
-        </form>
+      </form>
     </Box>
   );
 };
--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,24 +1,21 @@
-import {
-  Box,
-  Heading,
-} from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 import 'react-datepicker/dist/react-datepicker.css';
-import WithSubnavigation from '../components/Navbar'; 
+import WithSubnavigation from '../components/Navbar';
 import AppointmentSubmit from '../components/AppointmentForm';
 
 const AppointmentForm = () => {
   return (
     <Box>
-      <WithSubnavigation/>
+      <WithSubnavigation />
       <Box p={8} maxWidth="md" mx="auto">
         <Heading as="h2" size="xl" textAlign="center" mb={6}>
           Scheduling form
         </Heading>
         <Box shadow="base" rounded="lg" p={6}>
-        <AppointmentSubmit/>
+          <AppointmentSubmit />
         </Box>
       </Box>
-    </Box> 
+    </Box>
   );
 };
 
